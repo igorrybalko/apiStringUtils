3fb0ce520b4c9a8a08bf1e2d359092cb1348b352
Igor <igorrybalko2009@gmail.com>
2024-04-28 19:02:21 +0300

    opened page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,19 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <script src="https://cdn.tailwindcss.com"></script>
         <title>API StringUtils</title>
 
-   
     </head>
     <body class="antialiased">
-      
+        <div class="container mx-auto max-w-6xl px-3.5">
+            <div class="mt-6">
+                <p>It is an API service for the StringUtils project - <a class="underline" href="https://stringutils.online">https://stringutils.online</a></p>
+                <p>Project blog - <a class="underline" href="https://blog.stringutils.online">https://blog.stringutils.online</a></p>
+            </div>
+        </div>
     </body>
 </html>
