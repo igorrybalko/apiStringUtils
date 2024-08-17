6654ae7b2a0684c15b5d59bfaddd79db0bebdf1b
Igor <igorrybalko2009@gmail.com>
2024-08-17 11:59:39 +0300

    dev

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://stringutils.online'],
+    'allowed_origins' => ['https://stringutils.online', 'http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
