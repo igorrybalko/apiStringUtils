179d464a003d073ad8b63bc38c69f870647c2e66
Igor <igorrybalko2009@gmail.com>
2025-06-20 17:54:20 +0300

    fix and change icons

--- resources/js/components/base64/Base64DecodePage.vue
+++ resources/js/components/base64/Base64DecodePage.vue
@@ -50,25 +50,23 @@ const rules = reactive<FormRules<RuleForm>>({
 
 const result = ref("");
 
-function b64EncodeUnicode(str: string) {
-    // first we use encodeURIComponent to get percent-encoded UTF-8,
-    // then we convert the percent encodings into raw bytes which
-    // can be fed into btoa.
-    return btoa(
-        encodeURIComponent(str).replace(
-            /%([0-9A-F]{2})/g,
-            function toSolidBytes(_match, p1) {
-                return String.fromCharCode(parseInt("0x" + p1, 16));
-            }
-        )
-    );
-}
+function b64DecodeUnicode(str: string) {
+  // Going backwards: from bytestream, to percent-encoding, to original string.
+  return decodeURIComponent(
+   atob(str)
+    .split('')
+    .map(function (c) {
+     return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    })
+    .join('')
+  );
+ }
 
 const submitForm = (formEl: FormInstance | undefined) => {
     if (!formEl) return;
     formEl.validate((valid) => {
         if (valid) {
-            result.value = b64EncodeUnicode(form.text);
+            result.value = b64DecodeUnicode(form.text);
         }
     });
 };
